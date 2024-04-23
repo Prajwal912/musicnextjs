ba6b1c0e62f386d42a1f3c039a16f8a3a234ae57
prajwal <prajwalshah912@gmail.com>
2024-04-23 23:23:46 +0530

    data added for looping

--- src/app/chai/lemon/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const lemon = () => {
-  return (
-    <div>lemon</div>
-  )
-}
-
-export default lemon
--- src/app/chai/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const chai = () => {
-  return (
-    <div className="text-4xl text-center mt-8">chai page with next</div>
-  )
-}
-
-export default chai
